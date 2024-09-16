360ff62f3c76357cf512770dee6165f83cbe90e9
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-09-16 18:28:01 +0530

    add post javascript example

---
+++ 02expressTutorial/17post_javascript_ex/app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+let {people} = require('./data')
+
+const app = express()
+
+app.use(express.static('./public_methods')) // using static folders
+
+// parse form data
+app.use(express.urlencoded({extended:false}))
+// parse json data
+app.use(express.json()) // to access from forms     
+
+app.get('/api/people', (req, res) => {
+    res.json({success: true, data: people})         
+})  
+
+app.post('/api/people', (req, res) => {
+    // inspect -> network -> payload ( to see the data what we pased in form)
+
+    // console.log(req.body)
+    const {name} = req.body
+
+    if (!name) {
+        return      res.status(400).send({sucess: false, msg: 'please add some value'})
+    }
+    res.status(201).send({sucess: true, person: name}) // 201 is required for post 
+            
+})
+
+app.post('/login', (req, res) => {
+    // console.log(req.body) // [Object: null prototype] { name: '  demo' } 
+    // please note that, there is name in html form
+    const { name } = req.body;
+
+    if (name) {
+        return res.status(200).send(`Successful login for ${name}`)
+    }
+
+    return res.status(404).send('Error! Please add the user')  
+})                      
+
+
+
+
+
+app.listen(5000, ()=> {          
+    console.log('sever listening on 5000...')
+})
+
+/* output: 
+
+*/      
